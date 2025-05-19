dd133850cc3621dc4161d08801ffd1711cc1eaff
ArturoITSON <sergio.garcia233316@potros.itson.edu.mx>
2025-05-19 00:03:36 -0700

    Revert "Correcion bug de colocar barcos"
    This reverts commit 0b5134d5a5643642538597daaba39ce07d37386d.

--- SeaBattleModel/src/main/java/negocio/Tablero.java
+++ SeaBattleModel/src/main/java/negocio/Tablero.java
@@ -29,21 +29,19 @@ public class Tablero {
     public boolean colocarBarco(Barco barco, int x, int y) {
 
         Orientacion orientacion = barco.getOrientacion();
-        if (orientacion == Orientacion.VERTICAL) {
+        if (orientacion == Orientacion.HORIZONTAL) {
             if (y + barco.getTamaño() > 10) {
-                System.out.println("primer if");
                 return false;
             }
             for (int i = 0; i < barco.getTamaño(); i++) {
                 if (celdas[x][y + i].tieneBarco()) {
-                    System.out.println("segundo if");
                     return false; // Hay un barco en esta posición
                 }
             }
             for (int i = 0; i < barco.getTamaño(); i++) {
                 celdas[x][y + i].asignarBarco(barco); // Coloca el barco
             }
-        } else if (orientacion == Orientacion.HORIZONTAL) {
+        } else if (orientacion == Orientacion.VERTICAL) {
             if (x + barco.getTamaño() > 10) {
                 return false; // El barco no cabe
             }
